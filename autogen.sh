389531e1dc8938d7ff4ae10422cde155d4e5a3d8
laoshanxi <178029200@qq.com>
2019-11-06 21:07:16 +0800

    add fpm build step

--- autogen.sh
+++ autogen.sh
@@ -16,3 +16,17 @@ if [ -f "/usr/bin/apt" ]; then
 	sudo apt install -y nodejs
 	sudo apt install -y npm
 fi
+
+
+#RHEL
+if [ -f "/usr/bin/yum" ]; then
+	yum install -y make
+	yum install -y ruby rubygems ruby-devel
+	yum install -y rpm-build
+fi
+
+#Ubuntu
+if [ -f "/usr/bin/apt" ]; then
+	apt install -y make alien
+	apt install -y ruby ruby-dev rubygems
+fi
