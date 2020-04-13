74300d6e5b33da3aab778638bf5622a38cf38f12
laoshanxi <178029200@qq.com>
2020-04-13 13:06:14 +0800

    Update files.js

--- src/api/files.js
+++ src/api/files.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function upload(filepath, data) {
   return request({
-    url: '/appmgr/upload',
+    url: '/appmgr/file/upload',
     method: 'POST',
     headers:{
       "file_path" : filepath,
@@ -14,7 +14,7 @@ export function upload(filepath, data) {
 
 export function download(filepath) {
   return request({
-    url: '/appmgr/download',
+    url: '/appmgr/file/download',
     method: 'GET',
     headers:{
       "file_path" : filepath
