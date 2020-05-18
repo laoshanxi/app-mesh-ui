d4d4cacd39daa06c2e035670180d3989cf42b3c1
laoshanxi <178029200@qq.com>
2020-05-18 16:30:17 +0800

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -12,6 +12,6 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: true,
+  sidebarLogo: true
   // baseUrl: window.location.origin
 }
